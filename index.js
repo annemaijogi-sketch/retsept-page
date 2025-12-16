2bc1332520b0358a3bc2cfd88ad49f131f065e5f
annemaijogi-sketch <annemai.jogi@tktk.ee>
2025-12-16 23:20:36 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const containerElement = document.querySelector('.container');
+
+getRandomRecipe();
+
+async function getRandomRecipe() {
+    const response = await fetch('https://rke143week11.onrender.com/random');
+    const recipe = await response.json();
+
+    const recipeName = recipe.recipe.recipename;
+    const imageUrl = recipe.recipe.imageurl;
+    const recipeInstructions = recipe.recipe.instructions;
+    const ingredientsArray = recipe.recipe.ingredients;
+
+    const recipeNameTitle = document.createElement('h2');
+    const recipeImage = document.createElement('img');
+    const instructionsBlock = document.createElement('p');
+    const ingredientsList = document.createElement('ul');
+
+    ingredientsArray.forEach(ingredient => {
+        const li = document.createElement('li');
+        li.innerHTML = ingredient;
+        ingredientsList.appendChild(li);
+    });
+
+    recipeNameTitle.innerHTML = recipeName;
+    recipeImage.src = imageUrl;
+    instructionsBlock.innerHTML = recipeInstructions;
+
+    const footer = document.createElement('footer');
+    footer.innerHTML = '&#169 2023 IAmAwesome All Rights Reserved';
+
+    containerElement.appendChild(recipeNameTitle);
+    containerElement.appendChild(recipeImage);
+    containerElement.appendChild(ingredientsList);
+    containerElement.appendChild(instructionsBlock);
+    containerElement.appendChild(footer);
+}
